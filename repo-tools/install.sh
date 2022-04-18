efee8cc2b48c8bef60a03bed2d37a88c4274920e
Patrice Ferlet <metal3d@gmail.com>
2022-04-18 14:36:05 +0200

    Fix curl command

--- repo-tools/install.sh
+++ repo-tools/install.sh
@@ -36,6 +36,6 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # download the binary from latest release in github.com/metal3d/goreorder
-curl -L ${GIT_REPOSITORY}/${GIT_REPONAME}/releases/latest/download/${TARGET} -o $BIN_PATH/$BIN_NAME && \
+curl -L --progress-bar ${GIT_REPOSITORY}/${GIT_REPONAME}/releases/latest/download/${TARGET} -o $BIN_PATH/$BIN_NAME && \
     chmod +x $BIN_PATH/$BIN_NAME && \
     echo "goreorder has been installed to $BIN_PATH/$BIN_NAME"
